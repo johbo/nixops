dba2db4c0b84ed4602195b081b6c26b5abdcb207
Johannes Bornhold <johannes@bornhold.name>
2017-12-28 17:40:33 +0100

    Wait in container.stop until container is down
    Avoids trouble when calling start right after stop

--- nixops/backends/container.py
+++ nixops/backends/container.py
@@ -273,9 +273,15 @@ class ContainerState(MachineState):
 
     def stop(self):
         if not self.vm_id: return True
-        self.log("stopping container...")
+        self.log_continue("stopping container ...")
         self.state = self.STOPPING
         self.host_ssh.run_command("nixos-container stop {0}".format(self.vm_id))
+
+        while self._get_container_status() == "up":
+            time.sleep(1)
+            self.log_continue(".")
+
+        self.log_end(" stopped.")
         self.state = self.STOPPED
 
     def start(self):
